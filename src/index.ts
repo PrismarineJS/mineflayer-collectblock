29d4ddc6774c86f3fac8893382aa31c1051bf2d6
TheDudeFromCI <thedudefromci@gmail.com>
2020-10-21 01:08:56 -0700

    Auto resolve dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import { Bot } from "mineflayer";
-import { CollectBlock } from "./CollectBlock";
+import { Bot } from 'mineflayer';
+import { CollectBlock } from './CollectBlock';
+import { pathfinder } from 'mineflayer-pathfinder';
 
 export function plugin(bot: Bot)
 {
     // @ts-ignore
     bot.collectBlock = new CollectBlock(bot);
+
+    // Load pathfinder if not loaded manually.
+    setTimeout(() => loadPathfinder(bot), 0)
+}
+
+function loadPathfinder(bot: Bot)
+{
+    // @ts-ignore
+    if (bot.pathfinder) return;
+
+    bot.loadPlugin(pathfinder);
 }
