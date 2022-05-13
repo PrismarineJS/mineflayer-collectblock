a26f54c1c3ca462116736bc335d17dc2f71f4245
Alexander Ivanov <saiv46.dev@gmail.com>
2022-05-13 17:03:39 +0800

    Update CollectBlock.ts

--- src/CollectBlock.ts
+++ src/CollectBlock.ts
@@ -66,7 +66,6 @@ async function mineBlock (bot: Bot, block: Block, options: CollectOptionsFull): 
     return
   }
 
-  // @ts-expect-error
   await bot.tool.equipForBlock(block, equipToolOptions)
 
   // @ts-expect-error
@@ -236,7 +235,6 @@ export class CollectBlock {
       throw error('UnresolvedDependency', 'The mineflayer-collectblock plugin relies on the mineflayer-pathfinder plugin to run!')
     }
 
-    // @ts-expect-error
     if (this.bot.tool == null) {
       throw error('UnresolvedDependency', 'The mineflayer-collectblock plugin relies on the mineflayer-tool plugin to run!')
     }
