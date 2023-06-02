d059c2c33a331536d4d2223c2fd27f4ccd590b55
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-06-01 20:24:30 -0400

    Fix typescript errors

--- src/CollectBlock.ts
+++ src/CollectBlock.ts
@@ -9,7 +9,6 @@ import { emptyInventoryIfFull, ItemFilter } from './Inventory'
 import { findFromVein } from './BlockVeins'
 import { Collectable, Targets } from './Targets'
 import { Item } from 'prismarine-item'
-import mcDataLoader from 'minecraft-data'
 import { once } from 'events'
 import { callbackify } from 'util'
 
@@ -198,7 +197,7 @@ export class CollectBlock {
   constructor (bot: Bot) {
     this.bot = bot
     this.targets = new Targets(bot)
-    this.movements = new Movements(bot, mcDataLoader(bot.version))
+    this.movements = new Movements(bot)
   }
 
   /**
