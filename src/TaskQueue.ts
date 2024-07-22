9eb2e9a7ecfe768b77284e005f6855f8d1b89545
Cleboost <clement.balarot@gmail.com>
2024-07-22 10:20:55 +0200

    Revert "feat: indent"
    This reverts commit 99e97818a408d9202c79ef4ea2b23ef1f72037d3.

--- src/TaskQueue.ts
+++ src/TaskQueue.ts
@@ -1,77 +1,77 @@
-import type { Callback } from "./index";
-export type Task = (cb: Callback) => void;
-export type SyncTask = () => void;
+import type { Callback } from './index'
+export type Task = (cb: Callback) => void
+export type SyncTask = () => void
 
 /**
  * A simple utility class for queuing up a series of async tasks to execute.
  */
 export class TaskQueue {
-	private tasks: Task[] = [];
+  private tasks: Task[] = []
 
-	/**
-	 * If true, the task list will stop executing if one of the tasks throws an error.
-	 */
-	readonly stopOnError: boolean = true;
+  /**
+   * If true, the task list will stop executing if one of the tasks throws an error.
+   */
+  readonly stopOnError: boolean = true
 
-	/**
-	 * Adds a new async task to this queue. The provided callback should be executed when
-	 * the async task is complete.
-	 *
-	 * @param task - The async task to add.
-	 */
-	add(task: Task): void {
-		this.tasks.push(task);
-	}
+  /**
+   * Adds a new async task to this queue. The provided callback should be executed when
+   * the async task is complete.
+   *
+   * @param task - The async task to add.
+   */
+  add (task: Task): void {
+    this.tasks.push(task)
+  }
 
-	/**
-	 * Adds a synchronous task toi this queue.
-	 *
-	 * @param task - The sync task to add.
-	 */
-	addSync(task: SyncTask): void {
-		this.add((cb) => {
-			try {
-				task();
-				cb();
-			} catch (err: any) {
-				cb(err);
-			}
-		});
-	}
+  /**
+   * Adds a synchronous task toi this queue.
+   *
+   * @param task - The sync task to add.
+   */
+  addSync (task: SyncTask): void {
+    this.add((cb) => {
+      try {
+        task()
+        cb()
+      } catch (err: any) {
+        cb(err)
+      }
+    })
+  }
 
-	/**
-	 * Runs all tasks currently in this queue and empties the queue.
-	 *
-	 * @param cb - The optional callback to be executed when all tasks in this queue have
-	 * finished executing.
-	 */
-	runAll(cb?: Callback): void {
-		const taskList = this.tasks;
-		this.tasks = [];
+  /**
+   * Runs all tasks currently in this queue and empties the queue.
+   *
+   * @param cb - The optional callback to be executed when all tasks in this queue have
+   * finished executing.
+   */
+  runAll (cb?: Callback): void {
+    const taskList = this.tasks
+    this.tasks = []
 
-		let index = -1;
-		const runNext: () => void = () => {
-			index++;
-			if (index >= taskList.length) {
-				if (cb !== undefined) cb();
-				return;
-			}
+    let index = -1
+    const runNext: () => void = () => {
+      index++
+      if (index >= taskList.length) {
+        if (cb !== undefined) cb()
+        return
+      }
 
-			try {
-				taskList[index]((err) => {
-					if (err !== undefined) {
-						if (cb !== undefined) cb(err);
+      try {
+        taskList[index]((err) => {
+          if (err !== undefined) {
+            if (cb !== undefined) cb(err)
 
-						if (this.stopOnError) return;
-					}
+            if (this.stopOnError) return
+          }
 
-					runNext();
-				});
-			} catch (err: any) {
-				if (cb !== undefined) cb(err);
-			}
-		};
+          runNext()
+        })
+      } catch (err: any) {
+        if (cb !== undefined) cb(err)
+      }
+    }
 
-		runNext();
-	}
+    runNext()
+  }
 }
